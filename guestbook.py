54e90f162d384fc1e2e9c37df117e935818c41f8
Adam Miller <asm2242@rit.edu>
2013-06-30 00:23:36 -0400

    finished part 4

--- guestbook.py
+++ guestbook.py
@@ -2,9 +2,15 @@ import os
 import re
 import cgi
 from string import letters
+import random
+import string
+import hashlib
+import hmac
 
 import webapp2
 import jinja2
+
+SECRET = "FOST"
 
 from google.appengine.ext import db
 
@@ -15,6 +21,32 @@ jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
 def render_str(template, **params):
     t = jinja_env.get_template(template)
     return t.render(params)
+
+def make_salt():
+    return ''.join(random.choice(string.letters) for x in xrange(5))
+
+
+def make_pw_hash(name, pw, salt=None):
+    if salt == None:
+        salt = make_salt()
+    h = hashlib.sha256(name + pw + salt).hexdigest()
+    return '%s,%s' % (h, salt)
+
+def valid_pw(name, pw, h):
+    cats = h.split(",")
+    if make_pw_hash(name,pw,cats[1]) == h:
+        return True
+
+def hash_str(s):
+    return hmac.new(SECRET,s).hexdigest()
+
+def make_secure_val(s):
+    return "%s|%s" % (s, hash_str(s))
+
+def check_secure_val(h):
+    val = h.split('|')[0]
+    if h == make_secure_val(val):
+        return val
 
 form = """
 <form method="post">
@@ -62,11 +94,19 @@ class MainPage(webapp2.RequestHandler):
     def post(self):
 	#self.response.headers['Content-Type'] = 'text/plain'
 	self.response.write(form1 % self.wtta(self.request.get('text')))
-
+	
 	
 class WelcomePage(webapp2.RequestHandler):
+    ## add some shit to check the cookie
     def get(self):
-        self.response.write(render_str('welcome.html', username = self.request.get('username')))
+        #validate cookie
+        userdude = self.request.cookies.get('user_id')
+        #self.response.write(userdude)
+        name = check_secure_val(userdude)
+        if name:
+            self.response.write(render_str('welcome.html', username = name))
+        else:
+            self.redirect('/signup')
 
         
 class BlogPost(db.Model):
@@ -74,6 +114,11 @@ class BlogPost(db.Model):
     blog_content = db.TextProperty(required = True)
     date_created = db.DateTimeProperty(auto_now_add = True)
 
+class User(db.Model):
+    username = db.StringProperty(required = True)
+    password = db.StringProperty(required = True)
+
+    
 class BlogPage(webapp2.RequestHandler):
     def get(self):
         posts = db.GqlQuery('SELECT * from BlogPost order by date_created desc')
@@ -102,10 +147,33 @@ class NewPost(webapp2.RequestHandler):
             self.redirect('/blog/'+str(bp.key().id()) )
         else: 
             self.response.write(render_str('newpost.html', old_title = old_title, old_blog_post = obp))
+
+            
+class LogoutPage(webapp2.RequestHandler):
+    #nuke cookie
+    #redirect
+    def get(self):
+        self.response.headers.add_header('Set-Cookie',str('user_id=; Path=/'))
+        self.redirect('/signup')
+    
+            
+class LoginPage(webapp2.RequestHandler):
+    def get(self):
+        self.response.write(render_str('login.html'))
+    	
+    def post(self):
+        self.username = self.request.get('username')
+        self.password = self.request.get('password')
+        #user = User.all().filter('username= '+self.username).get()
+        user = db.GqlQuery("SELECT * FROM User WHERE username IN ('"+self.username+"')").get()
+        if user and valid_pw(self.username, self.password, user.password):
+            self.response.headers.add_header('Set-Cookie',str('user_id='+make_secure_val(self.request.get('username'))+'; Path=/'))
+            self.redirect('/welcome')
+        else:
+            self.response.write(render_str('login.html',login_error='Login Error'))
         
 
 class SignupPage(webapp2.RequestHandler):
-
     USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
     PW_RE = re.compile(r"^.{3,20}$")
     EMAIL_RE = re.compile(r"^[\S]+@[\S]+\.[\S]+$")
@@ -142,7 +210,7 @@ class SignupPage(webapp2.RequestHandler):
         self.response.write(render_str('signup.html'))
     	
     def post(self):
-        flag = True
+        vflag = True
         oldtext1 = self.request.get('username')
         usernameerror1 = ""
         passworderror1 = ""
@@ -150,28 +218,39 @@ class SignupPage(webapp2.RequestHandler):
         emailerror1 =""
         if not(self.valid_username(self.request.get('username'))):
             usernameerror1 = 'Thats not a valid username'
-            flag = False
+            vflag = False
         if not(self.valid_password(self.request.get('password'))):
             passworderror1 = 'Thats not a valid password'
-            flag = False
+            vflag = False
         # This should be the thing that checks they are the same
         if not(self.valid_password1(self.request.get('password'),self.request.get('verify'))):
             passworderror3 = 'Those passwords dont match'
-            flag = False
+            vflag = False
         #This just checks the email
         if not(self.valid_email(self.request.get('email'))):
             emailerror1 = "That is not a valid email"
-            flag = False
+            vflag = False
             
         #else:
-        if flag:
-            self.redirect('/welcome?username='+oldtext1)
+        if vflag:
+            key = db.Key.from_path('User', oldtext1)
+            if db.get(key):
+                self.response.write(render_str('signup.html', oldtext ='',usernameerror="User already exists.", passworderror= passworderror1, passworderror2=passworderror3,emailerror=emailerror1))
+            else:
+                
+                #self.redirect('/welcome?username='+self.request.get('password'))
+                newuser = User(username = oldtext1, password = make_pw_hash(oldtext1,self.request.get('password')))
+                newuser.put()
+                self.response.headers.add_header('Set-Cookie',str('user_id='+make_secure_val(self.request.get('username'))+'; Path=/'))
+                self.redirect('/welcome')
         else:
             self.response.write(render_str('signup.html', oldtext =oldtext1,usernameerror=usernameerror1
                                        , passworderror= passworderror1, passworderror2 =passworderror3,
                                        emailerror =emailerror1))
-		
+
+
 
 
 application = webapp2.WSGIApplication([('/', MainPage),('/signup', SignupPage),('/welcome', WelcomePage),('/blog/newpost', NewPost)
-                                       ,('/blog', BlogPage), ('/blog/([0-9]+)', PermaPost)], debug=True)
+                                       ,('/blog', BlogPage), ('/login', LoginPage), ('/logout', LogoutPage)
+                                       ,('/blog/([0-9]+)', PermaPost)], debug=True)
