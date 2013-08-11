008af659e2f65dc0db61253a0d802733b5de85d5
Adam Miller <adam.wulfgar@gmail.com>
2013-08-10 21:58:03 -0400

    added in some security

--- guestbook.py
+++ guestbook.py
@@ -1,3 +1,14 @@
+#-------------------------------------------------------------------------------
+# Name:        blog.py
+# Purpose:
+#
+# Author:      Millzor
+#
+# Created:     8/10/2013
+# Copyright:   (c) Millzor 2013
+#-------------------------------------------------------------------------------
+
+
 import os
 import re
 import cgi
@@ -23,7 +34,7 @@ PAGE_RE = r'(/(?:[a-zA-Z0-9_-]+/?)*)'
 
 template_dir = os.path.join(os.path.dirname(__file__), 'templates')
 jinja_env = jinja2.Environment(loader = jinja2.FileSystemLoader(template_dir),
-                               autoescape = False)
+                               autoescape = True)
 
 
 class MainHandler(webapp2.RequestHandler):
@@ -55,22 +66,25 @@ class MainHandler(webapp2.RequestHandler):
         return "%s|%s" % (s, self.hash_str(s))
 
     def check_secure_val(self, h):
-        val = h.split('|')[0]
-        if h == self.make_secure_val(val):
-            return val
+        if h != None:
+            val = h.split('|')[0]
+            if h == self.make_secure_val(val):
+                return val
+        return None
 
-        
+
+
 class HomePage(MainHandler):
     def get(self):
         self.response.out.write("mainpage")
 
-        
+
 class ROT13Handler(MainHandler):
 
     def get(self):
         #self.response.headers['Content-Type'] = 'text/plain'
         self.render("ROT13.html")
-        
+
     def wtta(self,s):
         #cypher all the letters
         news = ""
@@ -88,12 +102,12 @@ class ROT13Handler(MainHandler):
         news = cgi.escape(news)
         #return something that the user would see
         return news
-    	
+
     def post(self):
 	#self.response.headers['Content-Type'] = 'text/plain'
 	self.render("ROT13.html", secret_text = self.wtta(self.request.get('text')))
-	
-	
+
+
 class WelcomePage(MainHandler):
     ## add some shit to check the cookie
     def get(self):
@@ -104,9 +118,9 @@ class WelcomePage(MainHandler):
         if name:
             self.render('welcome.html', username = name)
         else:
-            self.redirect('/signup')
+            self.redirect('/blog')
 
-        
+
 class BlogPost(db.Model):
     title = db.StringProperty(required = True)
     blog_content = db.TextProperty(required = True)
@@ -123,32 +137,30 @@ class BlogPost(db.Model):
             }
         return d
 
-    
+
 class Flusher(MainHandler):
     def get(self):
-        memcache.flush_all()
-        self.redirect('/blog' )
-        
+        user = self.request.cookies.get('user_id')
+        if self.check_secure_val(user) != None:
+            memcache.flush_all()
+            self.redirect('/blog' )
+        else:
+            self.redirect('/blog' )
+
 
 class User(db.Model):
     username = db.StringProperty(required = True)
     password = db.StringProperty(required = True)
 
-
-
 class BlogPage(MainHandler):
-    global oldtime
-
-        
     def get(self):
         posts = memcache.get('front')
         if (posts == None):
-            posts = db.GqlQuery('SELECT * from BlogPost order by date_created desc')
+            posts = db.GqlQuery('SELECT * from BlogPost order by date_created desc limit 10')
             memcache.set('front', posts)
         self.render('blog.html', posts = posts)
 
 class PermaPost(MainHandler):
-    
     def get(self, post_id):
         ## search for the post in the database using GQL
         ## Use that post to generate a page with a single blog post.
@@ -158,49 +170,63 @@ class PermaPost(MainHandler):
             posts = db.get(key)
             memcache.set(str(post_id), posts)
         self.render('permalink.html', posts = posts)
-        
+
 
 class NewPost(MainHandler):
     def get(self):
-        self.render('newpost.html')
+        user = self.request.cookies.get('user_id')
+        if self.check_secure_val(user) != None:
+            self.render('newpost.html')
+        else:
+            self.response.out.write("Unauthorized User, punk!")
 
     def post(self):
-        old_title = self.request.get('subject')
-        obp = self.request.get('content')
-        if old_title and obp:
-            bp = BlogPost(title = old_title, blog_content = obp)
-            bp.put()
-            posts = db.GqlQuery('SELECT * from BlogPost order by date_created desc')
-            memcache.delete('front')
-            memcache.set('front', posts)
-            self.redirect('/blog/'+str(bp.key().id()))
-        else: 
-            self.render('newpost.html', old_title = old_title, old_blog_post = obp)
+        user = self.request.cookies.get('user_id')
+        if self.check_secure_val(user) != None:
+            old_title = self.request.get('subject')
+            obp = self.request.get('content')
+            if old_title and obp:
+                bp = BlogPost(title = old_title, blog_content = obp)
+                bp.put()
+                posts = db.GqlQuery('SELECT * from BlogPost order by date_created desc limit 10')
+                memcache.delete('front')
+                memcache.set('front', posts)
+                self.redirect('/blog/'+str(bp.key().id()))
+            else:
+                self.render('newpost.html', old_title = old_title, old_blog_post = obp)
+        else:
+            self.response.out.write("Unauthorized User, punk!")
 
-            
 class LogoutPage(MainHandler):
     #nuke cookie
     #redirect
     def get(self):
         self.response.headers.add_header('Set-Cookie',str('user_id=; Path=/'))
-        self.redirect('/wiki/signup')
-    
-class Perma2Json(webapp2.RequestHandler): 
+        self.redirect('/blog')
+
+class Perma2Json(webapp2.RequestHandler):
     # Implement Json here
     # Note: Will need the post ID to find in the database
     # Note:  Will need to use the small json parser to dumps()?
     def get(self, post_id):
         ## search for the post in the database using GQL
         ## Use that post to generate a page with a single blog post.
-        key = db.Key.from_path('BlogPost', int(post_id))
-        posts = db.get(key)
+        posts = memcache.get(str(post_id))
+        if (posts == None):
+            key = db.Key.from_path('BlogPost', int(post_id))
+            posts = db.get(key)
+            memcache.set(str(post_id), posts)
         jdog = json.dumps(posts.as_json())
         self.response.headers['Content-Type'] = 'application/json; charset=UTF-8'
         self.response.write(jdog)
-    
+
 class Blog2Json(webapp2.RequestHandler):
+    #This should be cached
     def get(self):
-        posts = db.GqlQuery('SELECT * from BlogPost order by date_created desc')
+        posts = memcache.get('front')
+        if (posts == None):
+            posts = db.GqlQuery('SELECT * from BlogPost order by date_created desc limit 10')
+            memcache.set('front', posts)
         jdog = json.dumps([p.as_json() for p in posts])
         self.response.headers['Content-Type'] = 'application/json; charset=UTF-8'
         self.response.write(jdog)
@@ -208,55 +234,55 @@ class Blog2Json(webapp2.RequestHandler):
 class LoginPage(MainHandler):
     def get(self):
         self.render('login.html')
-    	
+
     def post(self):
         self.username = self.request.get('username')
         self.password = self.request.get('password')
-        #user = User.all().filter('username= '+self.username).get()
         user = db.GqlQuery("SELECT * FROM User WHERE username IN ('"+self.username+"')").get()
         if user and self.valid_pw(self.username, self.password, user.password):
-            self.response.headers.add_header('Set-Cookie',str('user_id='+self.make_secure_val(self.request.get('username'))+'; Path=/'))
-            self.redirect('/welcome')
+            self.response.headers.add_header('Set-Cookie',
+            str('user_id='+ self.make_secure_val(self.request.get('username'))+'; Path=/'))
+            self.redirect('/blog/newpost')
         else:
-            self.render('login.html',login_error='Login Error')
-        
+            self.render('login.html',login_error='Incorrect username or password')
+
 
 class SignupPage(MainHandler):
     USER_RE = re.compile(r"^[a-zA-Z0-9_-]{3,20}$")
     PW_RE = re.compile(r"^.{3,20}$")
     EMAIL_RE = re.compile(r"^[\S]+@[\S]+\.[\S]+$")
-    
+
     ##
     # Validate the username
     # @param: username
-    # @returns: true if valid username, otherwise false    
+    # @returns: true if valid username, otherwise false
     def valid_username(self, username):
         return self.USER_RE.match(username)
 
     ##
     # Validate the password
     # @param: password
-    # @returns: true if valid paasword, otherwise false    
+    # @returns: true if valid paasword, otherwise false
     def valid_password(self, password):
         return self.PW_RE.match(password)
 
     ##
     # Validate the password
     # @param: password
-    # @returns: true if valid paasword, otherwise false    
+    # @returns: true if valid paasword, otherwise false
     def valid_password1(self, password, password1):
         return password == password1
 
     ##
     # Validate the email
     # @param: email
-    # @returns: true if valid email, otherwise false    
+    # @returns: true if valid email, otherwise false
     def valid_email(self, email):
         return self.EMAIL_RE.match(email) or email == ""
-    
+
     def get(self):
         self.render('signup.html')
-    	
+
     def post(self):
         vflag = True
         oldtext1 = self.request.get('username')
@@ -278,14 +304,17 @@ class SignupPage(MainHandler):
         if not(self.valid_email(self.request.get('email'))):
             emailerror1 = "That is not a valid email"
             vflag = False
-            
+
         #else:
         if vflag:
             key = db.Key.from_path('User', oldtext1)
             if db.get(key):
-                self.render('signup.html', oldtext ='',usernameerror="User already exists.", passworderror= passworderror1, passworderror2=passworderror3,emailerror=emailerror1)
+                self.render('signup.html', oldtext ='',
+                usernameerror="User already exists.",
+                passworderror= passworderror1,
+                passworderror2=passworderror3,emailerror=emailerror1)
             else:
-                
+
                 #self.redirect('/welcome?username='+self.request.get('password'))
                 newuser = User(username = oldtext1, password = self.make_pw_hash(oldtext1,self.request.get('password')))
                 newuser.put()
@@ -302,13 +331,13 @@ class SignupPage(MainHandler):
 application = webapp2.WSGIApplication([('/', HomePage),
                                         ('/rot13', ROT13Handler),
                                        #('/blog/signup', SignupPage),
-                                       #('/welcome', WelcomePage),
+                                       ('/welcome', WelcomePage),
                                        ('/blog/newpost', NewPost),
                                        ('/blog/flush', Flusher),
                                        ('/blog', BlogPage),
                                        ('/blog/login', LoginPage),
                                        ('/blog/logout', LogoutPage),
-                                       ('/blog/\.json', Blog2Json),
+                                       ('/blog\.json', Blog2Json),
                                        ('/blog/([0-9]+)', PermaPost),
                                        ('/blog/([0-9]+)\.json', Perma2Json),
 
